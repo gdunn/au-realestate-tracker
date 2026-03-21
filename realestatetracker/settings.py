dbedb9cb9996efbd03b9785975aa0abeb28f310b
Geoffrey Dunn <geoff@warmage.com>
2026-03-21 09:11:00 +0000

    Fix up static path

--- realestatetracker/settings.py
+++ realestatetracker/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "realestate_static/"
+STATIC_URL = "/realestate_static/"
 STATIC_ROOT = "/home/warmagec/misterpink.com.au/realestate_static/"
 
 # Authentication settings
